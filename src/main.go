2ce89aaff4130538e943bea0ce2eb823afd491d2
Detrivinog <detrivinog@gmail.com>
2022-01-25 20:41:05 -0500

    variables y constantes

---
+++ src/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println("Zero values: ", a, b, c, d)
+}
